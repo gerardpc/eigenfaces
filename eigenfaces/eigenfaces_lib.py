7aaaec15591640e9b4628546c36f38342a38a95b
gerardpc <gerardpc@pm.me>
2023-11-26 21:34:48 +0100

    Add ruff and lint

--- eigenfaces/eigenfaces_lib.py
+++ eigenfaces/eigenfaces_lib.py
@@ -1,7 +1,9 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
+"""Library for eigenfaces script."""
 import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
 
 
 def img_to_array(filepath: str, img_dim: tuple) -> np.ndarray:
@@ -16,17 +18,13 @@ def img_to_array(filepath: str, img_dim: tuple) -> np.ndarray:
     """
     img = Image.open(filepath)
     # image is converted to greyscale
-    img = img.convert('L') if img.mode != 'L' else img
-    img_data = np.asarray(img, dtype=np.float64).reshape(img_dim[0]*img_dim[1], 1)
+    img = img.convert("L") if img.mode != "L" else img
+    img_data = np.asarray(img, dtype=np.float64).reshape(img_dim[0] * img_dim[1], 1)
     return img_data
 
 
 def project_image(
-        filepath: str,
-        Phi: np.ndarray,
-        num_dim: int,
-        start_ef: int,
-        img_dim: tuple
+    filepath: str, Phi: np.ndarray, num_dim: int, start_ef: int, img_dim: tuple
 ) -> np.ndarray:
     """Project pics not in train dataset to first n eigenfaces.
 
@@ -42,17 +40,16 @@ def project_image(
     img_data = img_to_array(filepath, img_dim)
     img_data = img_data - img_data.mean()
     img_data = img_data / img_data.std()
-    return (img_data.T @ Phi[:, start_ef:start_ef + num_dim]).T
+    return (img_data.T @ Phi[:, start_ef : start_ef + num_dim]).T
 
 
 def images_to_A(
-        img_dim: tuple,
-        people: dict[str, str],
-        N: int,
-        show_avg_face: bool = False,
+    img_dim: tuple,
+    people: dict[str, str],
+    N: int,
+    show_avg_face: bool = False,
 ) -> np.ndarray:
-    """Load images, process them and stack in matrix as
-    column vectors.
+    """Load + process images and stack in matrix as column vectors.
 
     Args:
         img_dim: dimensions of the image in pixels
@@ -64,9 +61,9 @@ def images_to_A(
     avg = np.zeros((img_dim[0] * img_dim[1], 1))
     A = np.empty((img_dim[0] * img_dim[1], 0))
     # Load images and calculate average
-    for person, person_name in people.items():
+    for _, person_name in people.items():
         for j in range(1, N + 1):
-            file_name = f'faces/{person_name}{str(j).zfill(2)}.jpg'
+            file_name = f"faces/{person_name}{str(j).zfill(2)}.jpg"
             if os.path.exists(file_name):
                 R = img_to_array(file_name, img_dim=img_dim)
                 R = R - R.mean()
@@ -80,15 +77,15 @@ def images_to_A(
     # Calculate the "averaged" face (as m x n matrix)
     avgTS = np.reshape(avg, img_dim)
     if show_avg_face:
-        plt.imshow(avgTS, cmap='gray')
+        plt.imshow(avgTS, cmap="gray")
     # Center the sample pictures at the "origin"
     A_centered = A - avg
     return A_centered
 
 
 def plot_eigenfaces(
-        Phi: np.ndarray,
-        img_dim: tuple,
+    Phi: np.ndarray,
+    img_dim: tuple,
 ):
     """Plot first 9 eigenfaces.
 
@@ -104,16 +101,16 @@ def plot_eigenfaces(
             eigenface = np.reshape(Phi[:, count - 1], img_dim)
             # to give more contrast on plot we multiply by m x n
             eigenface = eigenface * img_dim[0] * img_dim[1]
-            ax[i, j].imshow(eigenface, cmap='gray')
+            ax[i, j].imshow(eigenface, cmap="gray")
             count += 1
 
 
 def plot_projections(
-        PER_1: np.ndarray,
-        PER_2: np.ndarray,
-        people: dict[str, str],
-        num_dim: int,
-        start_ef: int
+    PER_1: np.ndarray,
+    PER_2: np.ndarray,
+    people: dict[str, str],
+    num_dim: int,
+    start_ef: int,
 ) -> plt.axes:
     """Plot the first num_dim components of a face.
 
@@ -121,6 +118,7 @@ def plot_projections(
         PER_1: numpy vectors of person_1
         PER_2: numpy vectors of person_2
         num_dim: 2 or 3 (eigenfaces)
+        people: dictionary of person's names
         start_ef: integer to specify the first eigenface
             that gets projected. Eigenfaces used will be
             [start_ef:start_ef + num_dim]
@@ -129,22 +127,26 @@ def plot_projections(
         # 2 components plot
         fig = plt.figure()
         ax = fig.add_subplot()
-        ax.scatter(PER_1[0, :], PER_1[1, :], c='r', label=people["person_1"])
-        ax.scatter(PER_2[0, :], PER_2[1, :], c='b', label=people["person_2"])
-        ax.set_xlabel(f'eigenface {start_ef + 1}')
-        ax.set_ylabel(f'eigenface {start_ef + 2}')
+        ax.scatter(PER_1[0, :], PER_1[1, :], c="r", label=people["person_1"])
+        ax.scatter(PER_2[0, :], PER_2[1, :], c="b", label=people["person_2"])
+        ax.set_xlabel(f"eigenface {start_ef + 1}")
+        ax.set_ylabel(f"eigenface {start_ef + 2}")
         ax.set_title("2D projection on 2 eigenfaces")
         ax.legend()
 
     elif num_dim == 3:
         # 3 components plot
         fig = plt.figure()
-        ax = fig.add_subplot(projection='3d')
-        ax.scatter(PER_1[0, :], PER_1[1, :], PER_1[2, :], c='r', label=people["person_1"])
-        ax.scatter(PER_2[0, :], PER_2[1, :], PER_2[2, :], c='b', label=people["person_2"])
-        ax.set_xlabel(f'eigenface {start_ef + 1}')
-        ax.set_ylabel(f'eigenface {start_ef + 2}')
-        ax.set_zlabel(f'eigenface {start_ef + 3}')
+        ax = fig.add_subplot(projection="3d")
+        ax.scatter(
+            PER_1[0, :], PER_1[1, :], PER_1[2, :], c="r", label=people["person_1"]
+        )
+        ax.scatter(
+            PER_2[0, :], PER_2[1, :], PER_2[2, :], c="b", label=people["person_2"]
+        )
+        ax.set_xlabel(f"eigenface {start_ef + 1}")
+        ax.set_ylabel(f"eigenface {start_ef + 2}")
+        ax.set_zlabel(f"eigenface {start_ef + 3}")
         ax.set_title("3D projection on 3 eigenfaces")
         ax.legend()
     else:
--- eigenfaces/eigenfaces_script.py
+++ eigenfaces/eigenfaces_script.py
@@ -1,18 +1,10 @@
+"""This script is used to run the eigenfaces algorithm."""
 import matplotlib.pyplot as plt
 import numpy as np
-from eigenfaces_lib import (
-    images_to_A,
-    project_image,
-    plot_eigenfaces,
-    plot_projections
-)
-
+from eigenfaces_lib import images_to_A, plot_eigenfaces, plot_projections, project_image
 
 # People that will be used
-people = {
-    "person_1": 'stallone',
-    "person_2": 'taylor'
-}
+people = {"person_1": "stallone", "person_2": "taylor"}
 
 test_imgs = {
     "person_1": "faces/teststallone1.jpg",
@@ -29,9 +21,9 @@ N = 20
 # First eigenface to use for projection
 start_ef = 0
 
-#taylor arnold start_ef=4
-#stallone arnold start_ef=1
-#taylor stallone start_ef=1
+# taylor arnold start_ef=4
+# stallone arnold start_ef=1
+# taylor stallone start_ef=1
 
 
 # Print execution details on stdout
@@ -41,14 +33,13 @@ print(f"Number of images per person: {N}")
 print(f"First eigenface to use: {start_ef}")
 
 
-A_centered = images_to_A(show_avg_face=False, N=N,
-                         img_dim=img_dim, people=people)
+A_centered = images_to_A(show_avg_face=False, N=N, img_dim=img_dim, people=people)
 print("Images matrix shape:")
 print(A_centered.shape)
 
 # Computing the SVD
 U, S, Vt = np.linalg.svd(A_centered, full_matrices=False)
-Phi = U[:, :2*N]
+Phi = U[:, : 2 * N]
 
 # Plot Eigenfaces
 plot_eigenfaces(Phi=Phi, img_dim=img_dim)
@@ -56,17 +47,15 @@ plt.show()
 
 # Project faces of person_1 and person_2 to 3 first eigenfaces,
 # starting at start_ef
-PER_1 = (A_centered[:, :N].T @ Phi[:, start_ef:start_ef + 3]).T
-PER_2 = (A_centered[:, N:2 * N].T @ Phi[:, start_ef:start_ef + 3]).T
+PER_1 = (A_centered[:, :N].T @ Phi[:, start_ef : start_ef + 3]).T
+PER_2 = (A_centered[:, N : 2 * N].T @ Phi[:, start_ef : start_ef + 3]).T
 
 # plot in 2D
-plot_projections(num_dim=2, start_ef=start_ef,
-                 PER_1=PER_1, PER_2=PER_2, people=people)
+plot_projections(num_dim=2, start_ef=start_ef, PER_1=PER_1, PER_2=PER_2, people=people)
 plt.show()
 
 # plot in with one more eigenface, 3D
-plot_projections(num_dim=3, start_ef=start_ef,
-                 PER_1=PER_1, PER_2=PER_2, people=people)
+plot_projections(num_dim=3, start_ef=start_ef, PER_1=PER_1, PER_2=PER_2, people=people)
 plt.show()
 
 # Project test pictures not seen in SVD
@@ -75,22 +64,25 @@ per1_points = project_image(
     num_dim=3,
     start_ef=start_ef,
     Phi=Phi,
-    img_dim=img_dim
+    img_dim=img_dim,
 )
 per2_points = project_image(
     filepath=test_imgs["person_2"],
     num_dim=3,
     start_ef=start_ef,
     Phi=Phi,
-    img_dim=img_dim
+    img_dim=img_dim,
 )
 
 # Plotting the test points
-ax = plot_projections(num_dim=3, start_ef=start_ef,
-                      PER_1=PER_1, PER_2=PER_2, people=people)
-ax.scatter(per1_points[0], per1_points[1], per1_points[2], c='brown',
-           label=people["person_1"])
-ax.scatter(per2_points[0], per2_points[1], per2_points[2], c='aqua',
-           label=people["person_2"])
+ax = plot_projections(
+    num_dim=3, start_ef=start_ef, PER_1=PER_1, PER_2=PER_2, people=people
+)
+ax.scatter(
+    per1_points[0], per1_points[1], per1_points[2], c="brown", label=people["person_1"]
+)
+ax.scatter(
+    per2_points[0], per2_points[1], per2_points[2], c="aqua", label=people["person_2"]
+)
 ax.legend()
 plt.show()
